65cc549477bc2637a6d395af10777f45ec673d83
Michael Ernst <mernst@cs.washington.edu>
2009-08-22 22:40:54 -0700

    Consolidate some .jar files. Don't track generated .class files.

--- scripts/javarifier.bat
+++ scripts/javarifier.bat
@@ -7,4 +7,4 @@ set JAVARIFIER_DIR=%~d0
 set JAVARIFIER_DIR=%JAVARIFIER_DIR%%~p0
 set JAVARIFIER_LIB=%JAVARIFIER_DIR%lib
 
-java -cp "%JAVARIFIER_LIB%\javarifier.jar;%JAVARIFIER_LIB%\jasminclasses-2.2.3.jar;%JAVARIFIER_LIB%\polyglotclasses-1.3.2.jar;%JAVARIFIER_LIB%\sootclasses-2.2.3.jar" javarifier.Main "-defaultStubCPEntries" "%JAVARIFIER_DIR%annotated-jdk" "-defaultWorldCPEntries" "%CLASSPATH%" "-programCPEntries" "%CLASSPATH%" %*
+java -cp "%JAVARIFIER_LIB%\javarifier.jar" javarifier.Main "-defaultStubCPEntries" "%JAVARIFIER_DIR%annotated-jdk" "-defaultWorldCPEntries" "%CLASSPATH%" "-programCPEntries" "%CLASSPATH%" %*
