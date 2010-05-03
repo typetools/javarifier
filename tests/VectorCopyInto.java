c235b276cb9bf0892d33ba910e49e1798fb35971
Michael Ernst <mernst@cs.washington.edu>
2010-05-02 20:32:39 -0700

    Update goals to reflect array level fix

--- tests/VectorCopyInto.java
+++ tests/VectorCopyInto.java
@@ -2,10 +2,10 @@ import java.util.Vector;
 
 public class VectorCopyInto {
 
-  public String[] getMessageChain2(Vector list)
+  public String[] getMessageChain2(Vector v)
   {
-    String[] chain = new String[list.size()];
-    list.copyInto (chain);
+    String[] chain = new String[v.size()];
+    v.copyInto (chain);
     return chain;
   }
 
