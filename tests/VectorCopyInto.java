9f8e5df529822fdc9f07a416c339fa3b44befedd
Michael Ernst <mernst@cs.washington.edu>
2011-01-01 12:23:43 -0800

    Convert a variable from raw to generic

--- tests/VectorCopyInto.java
+++ tests/VectorCopyInto.java
@@ -2,7 +2,7 @@ import java.util.Vector;
 
 public class VectorCopyInto {
 
-  public String[] getMessageChain2(Vector v)
+  public String[] getMessageChain2(Vector<String> v)
   {
     String[] chain = new String[v.size()];
     v.copyInto (chain);
