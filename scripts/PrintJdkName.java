0b692354a8a2a737cbe22b0c17973c0deb3a5887
jonathangburke <jonathangburke@gmail.com>
2012-12-19 20:00:06 -0800

    Better handling for jdk versions

---
+++ scripts/PrintJdkName.java
@@ -0,0 +1,50 @@
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+/**
+ * Prints the value of property "java.home", which is the location
+ * where the JRE is installed.
+ */
+public class PrintJdkName {
+    public static void main(String[] args) {
+        System.out.println(findJdkJarName(getJreVersion()));
+    }
+
+    /**
+     * Extract the first two version numbers from java.version (e.g. 1.6 from 1.6.whatever)
+     * @return The first two version numbers from java.version (e.g. 1.6 from 1.6.whatever)
+     */
+    private static double getJreVersion() {
+        final Pattern versionPattern = Pattern.compile("^(\\d\\.\\d+)\\..*$");
+        final String  jreVersionStr = System.getProperty("java.version");
+        final Matcher versionMatcher = versionPattern.matcher(jreVersionStr);
+
+        final double version;
+        if(versionMatcher.matches()) {
+            version = Double.parseDouble(versionMatcher.group(1));
+        } else {
+            throw new RuntimeException("Could not determine version from property java.version=" + jreVersionStr);
+        }
+
+        return version;
+    }
+
+
+    /**
+     * Determine the version of the JRE that we are currently running and select a jdk<V>.jar where
+     * <V> is the version of java that is being run (e.g. 6, 7, ...)
+     * @return The jdk<V>.jar where <V> is the version of java that is being run (e.g. 6, 7, ...)
+     */
+    private static String findJdkJarName(final Double jreVersion) {
+        final String fileName;
+        if(jreVersion == 1.4 || jreVersion == 1.5 || jreVersion == 1.6) {
+            fileName = "jdk6.jar";
+        } else if(jreVersion == 1.7) {
+            fileName = "jdk7.jar";
+        } else {
+            throw new AssertionError("Unsupported JRE version: " + jreVersion);
+        }
+
+        return fileName;
+    }
+}
